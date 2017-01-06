88bb1825aaeb4c8808fa612eb1a400c12356359e
Jacob Longhurst <jlonghurst@dronosinc.com>
2017-01-06 10:38:55 -0700

    asdfa

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   SESSION_SECRET: "sxmnNYgwjLTxfJPpu2TS7qJM",
   PORT: 8080,
-  MONGO_URI: "mongodb://localhost:27017/trextrrcomingsoon"
+  MONGO_URI: "mongodb://localhost:27017/jacoblonghurst"
 };

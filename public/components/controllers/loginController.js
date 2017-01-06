8398a192bbf93cec731d5d7390ab28879d08e10f
Jacob Longhurst <jlonghurst@dronosinc.com>
2017-01-06 12:01:05 -0700

    Clean up stylesheet

--- public/components/controllers/loginController.js
+++ public/components/controllers/loginController.js
@@ -1,5 +1,12 @@
 angular.module('JacobLonghurst')
   .controller('LoginController', function($scope) {
     $scope.active = false;
-
+    var db = logindb;
+    var collection = credentials;
+    $scope.login = function(username, password) {
+      var verify = db.collection.find({'username': username});
+      if (verify) {
+        
+      }
+    }
   });

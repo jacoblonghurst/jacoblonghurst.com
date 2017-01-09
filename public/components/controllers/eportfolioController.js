bd4d102ab1c20cec71885edf2c8fa23404b45ee5
Jacob Longhurst <jlonghurst@dronosinc.com>
2017-01-09 16:28:32 -0700

    Add eportfolio page and controller

---
+++ public/components/controllers/eportfolioController.js
@@ -0,0 +1,6 @@
+angular.module('JacobLonghurst')
+  .controller('eportfolioController', function($scope) {
+    $scope.activePhysics = false;
+    $scope.activeHistory = false;
+    $scope.physics1010 = false;
+  });

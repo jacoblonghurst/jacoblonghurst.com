c54c8e3f5668c08cfced3bae6d9014d178d21860
Jacob Longhurst <jlonghurst@dronosinc.com>
2017-01-11 14:48:29 -0700

    Fix random things

--- public/components/controllers/eportfolioController.js
+++ public/components/controllers/eportfolioController.js
@@ -1,6 +1,14 @@
 angular.module('JacobLonghurst')
   .controller('eportfolioController', function($scope) {
+
     $scope.activePhysics = false;
     $scope.activeHistory = false;
     $scope.physics1010 = false;
+    $scope.showHidden = function() {
+      $scope.body = angular.element(document.querySelector('#body'));
+      physics1010 = true;
+      body.removeClass("body");
+      // Add code to select the body and allow overflow-y.
+      // Also add in here the setting of the physics object since i don't think an ng-click can have multiple functions on it.
+    }
   });
